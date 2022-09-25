701d0134410eb3cba61d74459a59ac8a0b06e1fc
WebsiteSOLA <otabek_qoldoshev133@mail.ru>
2022-09-25 10:00:21 +0500

    asdad

--- src/un-auth.js
+++ src/un-auth.js
@@ -1,15 +1,16 @@
 import { Route, Routes } from "react-router-dom"
 import Sign from "./components/sign/sign"
+import Access from "./pages/access/access"
 import Main from "./pages/main/main"
 import NotFound from "./pages/not-found/not-found"
-import MainSign from "./pages/sign/main-sign"
+
 
 
 const UnAuthApp = () => {
     return (
         <Routes>
             <Route path="/" element={<Main />} />
-            <Route path="/main-sign" element={<MainSign />} />
+            <Route path="/access" element={<Access />} />
             <Route path="/sign" element={<Sign />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
