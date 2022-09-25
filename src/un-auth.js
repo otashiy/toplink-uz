9641cc2ed735403e4736e00a73a5fcaa0056a217
WebsiteSOLA <otabek_qoldoshev133@mail.ru>
2022-09-25 09:34:18 +0500

    corrected un-auth.js

--- src/un-auth.js
+++ src/un-auth.js
@@ -1,14 +1,15 @@
 import { Route, Routes } from "react-router-dom"
 import Sign from "./components/sign/sign"
+import Main from "./pages/main/main"
 import NotFound from "./pages/not-found/not-found"
 
 
 const UnAuthApp = () => {
     return (
         <Routes>
+            <Route path="/" element={<Main />} />
             <Route path="/sign" element={<Sign />} />
             <Route path="*" element={<NotFound />} />
-            <Route />
         </Routes>
     )
 }
